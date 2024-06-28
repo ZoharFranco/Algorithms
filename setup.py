b87f9071c7b37646b308402d6697aee4cd17f5e0
ZoharFranco <57002015+ZoharFranco@users.noreply.github.com>
2024-06-28 22:22:24 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='algpy',
+    version='0.1.0',    
+    description='A powerful algorithms package',
+    url='',
+    author='Zohar franco',
+    author_email='',
+    packages=find_packages(), 
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+       'Topic :: Software Development :: Algo Tools',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='CS data data structures algorithms algo',
+)
